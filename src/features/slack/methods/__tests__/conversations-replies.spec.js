336ec4aa4dd5f4f69a80d81963281dc3fdded3e9
Alexandre Lim <alexandre.lim94@gmail.com>
2021-04-18 16:55:41 +0200

    test(conversations-replies): add test for getRepliesFromConversationsHistoryMessages

--- src/features/slack/methods/__tests__/conversations-replies.spec.js
+++ src/features/slack/methods/__tests__/conversations-replies.spec.js
@@ -1,6 +1,6 @@
 import { WebClient } from '@slack/web-api';
 import { createCustomError } from '../../../../utils/error-handler';
-import { getConversationsReplies } from '../conversations-replies';
+import { getConversationsReplies, getRepliesFromConversationsHistoryMessages } from '../conversations-replies';
 
 jest.mock('../utils/slack-api-token', () => require('../utils/__mocks__/slack-api-token'));
 
@@ -86,5 +86,156 @@ describe('getConversationsReplies', () => {
 
     expect(conversationsRepliesError).toEqual(expectedError);
     expect(conversationsRepliesError).toHaveProperty('details', {});
+  });
+});
+
+describe('getRepliesFromConversationsHistoryMessages', () => {
+  test('should return an empty array if function is called with no param', async () => {
+    const { replies, error: repliesError } = await getRepliesFromConversationsHistoryMessages();
+    expect(WebClient).toBeCalledTimes(0);
+    expect(repliesError).toEqual({
+      hasError: false,
+      trace: null,
+    });
+    expect(replies).toEqual([]);
+  });
+
+  test('should return an array of conversationsReplies on success', async () => {
+    const conversationsHistoryMessages = [
+      {
+        type: 'message',
+        user: 'U061F7AUR',
+        text: 'island',
+        thread_ts: '1482960137.003543',
+        reply_count: 3,
+        subscribed: true,
+        last_read: '1484678597.521003',
+        unread_count: 0,
+        ts: '1482960137.003543',
+      },
+      {
+        type: 'message',
+        user: 'U061F7AUR',
+        text: 'one island',
+        thread_ts: '1482960137.003543',
+        parent_user_id: 'U061F7AUR',
+        ts: '1483076303.017503',
+      },
+      {
+        type: 'message',
+        user: 'U061F7AUR',
+        text: 'Message with no thread_ts',
+        ts: '1483076303.017503',
+      },
+    ];
+
+    const repliesResultsMock = {
+      ok: true,
+      messages: [{}],
+      has_more: true,
+      response_metadata: {
+        next_cursor: 'bmV4dF90czoxNTEyMDg1ODYxMDAwNTQz',
+      },
+    };
+
+    const webClientMock = {
+      conversations: {
+        replies: () => repliesResultsMock,
+      },
+    };
+
+    WebClient.mockReturnValue(webClientMock);
+
+    const { replies, error: repliesError } = await getRepliesFromConversationsHistoryMessages(
+      conversationsHistoryMessages
+    );
+    expect(WebClient).toBeCalledTimes(2);
+    expect(repliesError).toEqual({
+      hasError: false,
+      trace: null,
+    });
+    expect(replies).toEqual([repliesResultsMock, repliesResultsMock]);
+  });
+
+  test('should return an object error on second loop', async () => {
+    const conversationsHistoryMessages = [
+      {
+        type: 'message',
+        user: 'U061F7AUR',
+        text: 'island',
+        thread_ts: '1482960137.003543',
+        reply_count: 3,
+        subscribed: true,
+        last_read: '1484678597.521003',
+        unread_count: 0,
+        ts: '1482960137.003543',
+      },
+      {
+        type: 'message',
+        user: 'U061F7AUR',
+        text: 'one island',
+        thread_ts: '1482960137.003543',
+        parent_user_id: 'U061F7AUR',
+        ts: '1483076303.017503',
+      },
+      {
+        type: 'message',
+        user: 'U061F7AUR',
+        text: 'one island',
+        thread_ts: '1482960137.003543',
+        parent_user_id: 'U061F7AUR',
+        ts: '1483076303.017503',
+      },
+    ];
+
+    const repliesResultsMock = {
+      ok: true,
+      messages: [{}],
+      has_more: true,
+      response_metadata: {
+        next_cursor: 'bmV4dF90czoxNTEyMDg1ODYxMDAwNTQz',
+      },
+    };
+
+    const webClientMock = {
+      conversations: {
+        replies: () => repliesResultsMock,
+      },
+    };
+
+    const slackErrorMock = {
+      code: 'slack_webapi_platform_error',
+      data: {
+        ok: false,
+        error: 'thread_not_found',
+        response_metadata: {},
+      },
+    };
+    const message = 'Error returned by slack api when calling method conversations replies';
+    const expectedError = {
+      hasError: true,
+      trace: createCustomError({
+        message: { error: message, timestamp: conversationsHistoryMessages[1].ts },
+        details: slackErrorMock.data,
+      }),
+    };
+    const webClientErrorMock = {
+      conversations: {
+        replies: () => {
+          throw slackErrorMock;
+        },
+      },
+    };
+
+    WebClient.mockReturnValueOnce(webClientMock)
+      .mockReturnValueOnce(webClientErrorMock)
+      .mockReturnValueOnce(webClientMock);
+
+    const { replies, error: repliesError } = await getRepliesFromConversationsHistoryMessages(
+      conversationsHistoryMessages
+    );
+    expect(WebClient).toBeCalledTimes(2);
+    expect(repliesError).toEqual(expectedError);
+    expect(replies).toEqual([repliesResultsMock]);
   });
 });
