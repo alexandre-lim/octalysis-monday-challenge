8cdfa60c99f5db7cb7b96bc2ef3e28b96b96889e
Alexandre Lim <alexandre.lim94@gmail.com>
2021-04-29 17:50:56 +0200

    feat(mongo): add mongo middleware and add others middleware

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import logger from 'morgan';
+import cors from 'cors';
 import { initEnvVar } from './utils/env';
 import { errorHandler } from './utils/error-handler';
 import { slackRouter } from './features/slack/middlewares';
 import { jsonRouter } from './features/json/middlewares';
+import { expressMongoMiddleware } from './express-mongo';
 
 initEnvVar();
 
 const app = express();
 const dev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(expressMongoMiddleware);
+app.use(express.json());
+app.use(express.urlencoded());
 
 if (dev) {
   app.use(logger('dev'));
