654a4db62f86f78d0837d98f07e58257ed838992
Alexandre Lim <alexandre.lim94@gmail.com>
2021-05-03 21:27:21 +0200

    feat(mongo-replies): add insert replies route

--- src/features/mongo/middlewares/routes.js
+++ src/features/mongo/middlewares/routes.js
@@ -1,7 +1,12 @@
 import { getMongDb } from '../utils/mongo-db';
-import { getAllConversationsHistory } from '../../slack/methods/conversations-history';
+import {
+  getAllConversationsHistory,
+  getMessagesFromConversationsHistory,
+} from '../../slack/methods/conversations-history';
 import { createCustomError } from '../../../utils/error-handler';
 import { insertHistoryByDate } from '../../../services/history';
+import { getRepliesFromConversationsHistoryMessages } from '../../slack/methods/conversations-replies';
+import { insertRepliesByTimestamp } from '../../../services/replies';
 
 async function insertConversationsAllHistoryRoute(req, res, next) {
   const { allConversationsHistory, error } = await getAllConversationsHistory();
@@ -20,4 +25,29 @@ async function insertConversationsAllHistoryRoute(req, res, next) {
   error.hasError === true ? next(error.trace) : res.json(allConversationsHistory);
 }
 
-export { insertConversationsAllHistoryRoute };
+async function insertConversationsRepliesRoute(req, res, next) {
+  const { allConversationsHistory, error: conversationsHistoryError } = await getAllConversationsHistory();
+
+  if (conversationsHistoryError.hasError) return next(conversationsHistoryError.trace);
+
+  const conversationsHistoryMessages = getMessagesFromConversationsHistory(allConversationsHistory);
+
+  const { replies, error: repliesError } = await getRepliesFromConversationsHistoryMessages(
+    conversationsHistoryMessages
+  );
+
+  if (repliesError.hasError) return next(repliesError.trace);
+
+  if (repliesError.hasError === false) {
+    const db = getMongDb(req);
+    if (db) {
+      const results = await insertRepliesByTimestamp(db, replies);
+      return res.json(results);
+    } else {
+      const error = createCustomError({ message: 'No mongo database instance found in req' });
+      return next(error);
+    }
+  }
+}
+
+export { insertConversationsAllHistoryRoute, insertConversationsRepliesRoute };
--- src/tests/integrations/fake.js
+++ src/tests/integrations/fake.js
@@ -1,3 +1,182 @@
 export const fakeDate = '2021-04-26';
 export const fakeFilename = '1518829243.102800';
 export const fakeFileContent = { test: 'test' };
+
+export const fakeReplies = {
+  ok: true,
+  messages: [
+    {
+      type: 'message',
+      text: '*Question 1*\nWhich Core Drives are at play when we are driven to spend time in nature to relax?',
+      files: [
+        {
+          id: 'F018B0MC5DG',
+          created: 1596633612,
+          timestamp: 1596633612,
+          name: '13 - August 3 2020 - A Wizardry Vacation 2.jpeg',
+          title: '13 - August 3 2020 - A Wizardry Vacation 2.jpeg',
+          mimetype: 'image/jpeg',
+          filetype: 'jpg',
+          pretty_type: 'JPEG',
+          user: 'UERAASA4A4A',
+          editable: false,
+          size: 3192400,
+          mode: 'hosted',
+          is_external: false,
+          external_type: '',
+          is_public: true,
+          public_url_shared: false,
+          display_as_bot: false,
+          username: '',
+          url_private:
+            'https://files.slack.com/files-pri/T1MTYGXBK-F018B0MC5DG/13_-_august_3_2020_-_a_wizardry_vacation_2.jpeg',
+          url_private_download:
+            'https://files.slack.com/files-pri/T1MTYGXBK-F018B0MC5DG/download/13_-_august_3_2020_-_a_wizardry_vacation_2.jpeg',
+          thumb_64:
+            'https://files.slack.com/files-tmb/T1MTYGXBK-F018B0MC5DG-454cbae47d/13_-_august_3_2020_-_a_wizardry_vacation_2_64.jpg',
+          thumb_80:
+            'https://files.slack.com/files-tmb/T1MTYGXBK-F018B0MC5DG-454cbae47d/13_-_august_3_2020_-_a_wizardry_vacation_2_80.jpg',
+          thumb_360:
+            'https://files.slack.com/files-tmb/T1MTYGXBK-F018B0MC5DG-454cbae47d/13_-_august_3_2020_-_a_wizardry_vacation_2_360.jpg',
+          thumb_360_w: 360,
+          thumb_360_h: 270,
+          thumb_480:
+            'https://files.slack.com/files-tmb/T1MTYGXBK-F018B0MC5DG-454cbae47d/13_-_august_3_2020_-_a_wizardry_vacation_2_480.jpg',
+          thumb_480_w: 480,
+          thumb_480_h: 360,
+          thumb_160:
+            'https://files.slack.com/files-tmb/T1MTYGXBK-F018B0MC5DG-454cbae47d/13_-_august_3_2020_-_a_wizardry_vacation_2_160.jpg',
+          thumb_720:
+            'https://files.slack.com/files-tmb/T1MTYGXBK-F018B0MC5DG-454cbae47d/13_-_august_3_2020_-_a_wizardry_vacation_2_720.jpg',
+          thumb_720_w: 720,
+          thumb_720_h: 540,
+          thumb_800:
+            'https://files.slack.com/files-tmb/T1MTYGXBK-F018B0MC5DG-454cbae47d/13_-_august_3_2020_-_a_wizardry_vacation_2_800.jpg',
+          thumb_800_w: 800,
+          thumb_800_h: 600,
+          thumb_960:
+            'https://files.slack.com/files-tmb/T1MTYGXBK-F018B0MC5DG-454cbae47d/13_-_august_3_2020_-_a_wizardry_vacation_2_960.jpg',
+          thumb_960_w: 960,
+          thumb_960_h: 720,
+          thumb_1024:
+            'https://files.slack.com/files-tmb/T1MTYGXBK-F018B0MC5DG-454cbae47d/13_-_august_3_2020_-_a_wizardry_vacation_2_1024.jpg',
+          thumb_1024_w: 1024,
+          thumb_1024_h: 768,
+          original_w: 4032,
+          original_h: 3024,
+          thumb_tiny:
+            'AwAkADB7woAOCR6g0wWxJyWwv61IpZQOh/GhtwySo5qhDhCoQruIX2puVVVReg65pjkt0GPxpEiLHLHigQodAxIGKYW54qdVUAhBgdzTdsZJyP1oAi86bBzbjPbBpRPJ/FAc/Wpc+1ITUjGefIP+WB/Sk86UjmL9afmkPsxGaLgJ5smP9SfzpPNk/wCeIp6htx+YkemKDwMlqLjsBHFNpx6U2gBcUxXYgnOOKeKiT7rUgJVc7WBwfrTDz1pV6NSUhn//2Q==',
+          permalink:
+            'https://octalysisprime.slack.com/files/UERKYSD4Z/F018B0MC5DG/13_-_august_3_2020_-_a_wizardry_vacation_2.jpeg',
+          permalink_public: 'https://slack-files.com/T1MTYGXBK-F018B0MC5DG-272f03dd83',
+          is_starred: false,
+          has_rich_preview: false,
+        },
+      ],
+      upload: false,
+      blocks: [
+        {
+          type: 'rich_text',
+          block_id: '+o70n',
+          elements: [
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Question 1',
+                  style: {
+                    bold: true,
+                  },
+                },
+                {
+                  type: 'text',
+                  text: '\nWhich Core Drives are at play when we are driven to spend time in nature to relax?',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      user: 'UERAASA4A4A',
+      display_as_bot: false,
+      ts: '1596633627.082800',
+      thread_ts: '1596633627.082800',
+      reply_count: 11,
+      reply_users_count: 10,
+      latest_reply: '1597239576.119400',
+      reply_users: ['UJDNLT45AW', 'UERAASA4A4A', 'U018aa72X9A'],
+      is_locked: false,
+      subscribed: false,
+    },
+    {
+      client_msg_id: 'bb482894-773b-414c-992c-9fd34f6bd4ec',
+      type: 'message',
+      text: 'Share your relaxing thoughts here :relaxed:!',
+      user: 'UERAASA4A4A',
+      ts: '1596633768.084000',
+      team: 'T1MAAGXBK',
+      blocks: [
+        {
+          type: 'rich_text',
+          block_id: 'cMs',
+          elements: [
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text: 'Share your relaxing thoughts here ',
+                },
+                {
+                  type: 'emoji',
+                  name: 'relaxed',
+                },
+                {
+                  type: 'text',
+                  text: '!',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      thread_ts: '1596633627.082800',
+      parent_user_id: 'URIKYSD4Z',
+    },
+    {
+      client_msg_id: 'EC2F36FF-FABB-487C-95F2-9D74675CC0A4',
+      type: 'message',
+      text:
+        'I feel for a lot of people it is a removal CD8.  Many of the black hat triggers are removed in nature so it feels relaxing. Someone told me that we like seeing the ocean too because subconsciously we know it’s full open space and there is no danger. No dangerous animals that will jump out and eat us. ',
+      user: 'U1STAAH7Y',
+      ts: '1596945802.096800',
+      team: 'T1MTAAXBK',
+      blocks: [
+        {
+          type: 'rich_text',
+          block_id: 'kpb',
+          elements: [
+            {
+              type: 'rich_text_section',
+              elements: [
+                {
+                  type: 'text',
+                  text:
+                    'I feel for a lot of people it is a removal CD8.  Many of the black hat triggers are removed in nature so it feels relaxing. Someone told me that we like seeing the ocean too because subconsciously we know it’s full open space and there is no danger. No dangerous animals that will jump out and eat us. ',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      thread_ts: '1596633627.082800',
+      parent_user_id: 'ALRL484ASS',
+    },
+  ],
+  has_more: false,
+  response_metadata: {
+    scopes: ['channels:history', 'channels:read', 'groups:history', 'users:read'],
+    acceptedScopes: ['channels:history', 'groups:history', 'mpim:history', 'im:history', 'read'],
+  },
+};
