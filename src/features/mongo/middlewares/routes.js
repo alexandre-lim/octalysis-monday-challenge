9ccc630858f83e9290bd893531dfd43c1dfb7743
Alexandre Lim <alexandre.lim94@gmail.com>
2021-05-08 10:55:41 +0200

    feat(latest-messages): add route to insert latest messages in mongo

--- src/features/mongo/middlewares/routes.js
+++ src/features/mongo/middlewares/routes.js
@@ -1,6 +1,7 @@
 import { getMongDb } from '../utils/mongo-db';
 import {
   getAllConversationsHistory,
+  getConversationsHistory,
   getMessagesFromConversationsHistory,
 } from '../../slack/methods/conversations-history';
 import { createCustomError } from '../../../utils/error-handler';
@@ -8,6 +9,7 @@ import { insertHistoryByDate, findLatestHistory } from '../../../services/histor
 import { getRepliesFromConversationsHistoryMessages } from '../../slack/methods/conversations-replies';
 import { insertRepliesByTimestamp } from '../../../services/replies';
 import { getHistoryMessagesWithReplies, insertMessagesByTimestamp } from '../../../services/messages';
+import { getDateFromTwoWeeksAgo } from '../../../utils/date';
 
 async function insertConversationsAllHistoryRoute(req, res, next) {
   const { allConversationsHistory, error } = await getAllConversationsHistory();
@@ -72,4 +74,39 @@ async function insertMessagesRoute(req, res, next) {
   }
 }
 
-export { insertConversationsAllHistoryRoute, insertConversationsRepliesRoute, insertMessagesRoute };
+async function insertLatestMessagesRoute(req, res, next) {
+  const db = getMongDb(req);
+  if (db) {
+    const { intervalDate, twoWeeksAgoTimestamp } = getDateFromTwoWeeksAgo();
+
+    const conversationsHistory = await getConversationsHistory(null, twoWeeksAgoTimestamp);
+
+    if (conversationsHistory?.error === true) return next(conversationsHistory);
+
+    const conversationsHistoryMessages = getMessagesFromConversationsHistory([conversationsHistory]);
+
+    const {
+      replies,
+      historyMessagesWithReplies,
+      error: repliesError,
+    } = await getRepliesFromConversationsHistoryMessages(conversationsHistoryMessages);
+
+    if (repliesError.hasError) return next(repliesError.trace);
+
+    const resultInsertReplies = await insertRepliesByTimestamp(db, replies);
+
+    const resultInsertMessages = await insertMessagesByTimestamp(db, historyMessagesWithReplies);
+
+    res.json({ intervalDate, resultInsertMessages, resultInsertReplies });
+  } else {
+    const error = createCustomError({ message: 'No mongo database instance found in req' });
+    return next(error);
+  }
+}
+
+export {
+  insertConversationsAllHistoryRoute,
+  insertConversationsRepliesRoute,
+  insertMessagesRoute,
+  insertLatestMessagesRoute,
+};
